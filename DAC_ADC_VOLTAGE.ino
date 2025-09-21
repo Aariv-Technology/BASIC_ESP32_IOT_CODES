ae114b464033c87cead6510c9a21ad88fba5df54
Aariv Technology <aarivtechnology.23@gmail.com>
2025-09-21 16:21:40 +0530

    Add files via upload

---
+++ DAC_ADC_VOLTAGE.ino
@@ -0,0 +1,43 @@
+#define dac 26
+#define adc 34
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(adc, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  for(int i=0; i<=255; i++)
+  {
+    dacWrite(dac,i);
+    float adcValue = analogRead(adc);
+
+    Serial.print("ADC Value = ");
+    Serial.println(adcValue);
+
+    float voltage = adcValue/4095*3.3;
+    Serial.print("Voltage Value = ");
+    Serial.println(voltage);
+
+    delay(100);
+  }
+  delay(3000);
+  for(int i=255; i>=0; i--)
+  {
+    dacWrite(dac,i);
+    float adcValue = analogRead(adc);
+
+    Serial.print("ADC Value = ");
+    Serial.println(adcValue);
+
+    float voltage = adcValue/4095*3.3;
+    Serial.print("Voltage Value = ");
+    Serial.println(voltage);
+
+    delay(100);
+  }
+
+}
