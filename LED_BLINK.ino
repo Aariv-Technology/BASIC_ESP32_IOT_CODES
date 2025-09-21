fa855dd3dfe064450cd3c1b75c580fb1a6b28484
Aariv Technology <aarivtechnology.23@gmail.com>
2025-09-21 16:19:29 +0530

    Add files via upload

---
+++ LED_BLINK.ino
@@ -0,0 +1,14 @@
+// led blink for every 1s
+
+#define led 2
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+}
