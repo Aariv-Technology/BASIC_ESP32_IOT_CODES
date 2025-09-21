dd598d46690759bd7fa70cdc047f4947530b9595
Aariv Technology <aarivtechnology.23@gmail.com>
2025-09-21 16:24:23 +0530

    Add files via upload

---
+++ PWM.ino
@@ -0,0 +1,20 @@
+#define pwmLed 18
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pwmLed, OUTPUT);
+  analogWriteFrequency(pwmLed, 1000);
+  analogWriteResolution(pwmLed, 12);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i <= 4095; i++) {
+    analogWrite(pwmLed, i);
+    delay(30);
+  }
+  for (int i = 4095; i >= 0; i--) {
+    analogWrite(pwmLed, i);
+    delay(30);
+  }
+}
