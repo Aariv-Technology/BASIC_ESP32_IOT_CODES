cf9203292b6f09c149ac287022cd3816650ba3e5
Aariv Technology <aarivtechnology.23@gmail.com>
2025-09-21 16:24:48 +0530

    Add files via upload

---
+++ ULTRASONIC.ino
@@ -0,0 +1,23 @@
+#define trig 26
+#define echo 18
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+}
+
+void loop() {
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+  int duration = pulseIn(echo,HIGH);
+
+  float distance = 0.0343*duration/2;
+  Serial.print("Distance = ");
+  Serial.println(distance);
+  delay(10);
+}
