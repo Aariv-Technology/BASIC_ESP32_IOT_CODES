fa4828f6853f4b812952ec27919db9dbfe80878a
Aariv Technology <aarivtechnology.23@gmail.com>
2025-09-28 12:48:45 +0530

    Update WIFI_STA.ino

--- WIFI_STA.ino
+++ WIFI_STA.ino
@@ -1,10 +1,13 @@
 #include <WiFi.h>
 
+#define led 2
+
 const char* ssid = "YOUR_SSID";
 const char* password = "YOUR_PASSWORD";
 
 void setup() {
   Serial.begin(115200);
+  pinMode(led, OUTPUT);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
@@ -12,8 +15,10 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
+    digitalWrite(led, LOW);
   }
   Serial.println("\nConnected!");
+  digitalWrite(led, HIGH);
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
 }
